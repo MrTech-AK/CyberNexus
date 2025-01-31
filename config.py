96bf4832cc8335c8746ccd1a39cf54580d3efe10
Akshat <162790900+MrTech-AK@users.noreply.github.com>
2025-01-31 20:42:43 +0530

    Update and rename setup_config.py to config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+API_ID = 2285397  # Replace with your actual API ID
+API_HASH = "636fc37d9384a91c61619953cd760b48"  # Replace with your actual API hash
+STRING_SESSION = "1BVtsOKEBuy8xjgeErJcjzUTJPA34XnmrdcA84qQosi5KV-8qRVIBq3SoT8KjA6WNwGfVzNil-wzstyZ0dQDct6xRQNKoSDflNP6rCsPXrlwJ8X4foATYlzTkaJB0_PHmB4Rj_0y4IUkpe5PRaejGnxkfBp71HPv_6f9sXV-34Xl-UYcVMSuyzDwUA10veUFCJc90dLVKfej8pqNPi13qEbWCWi2SBEwPelFIgVOJI3Ik-HFP0e0mYIBLk8nyKi2wtUZK8rD4xLCawymNtwQETdc1Um3aripn4C23iozzX-UnbNUXlYKY3bc9RkcAmKCHngE40nAILH65j3EGCeLGwQHMPwWvpcQ="  # Replace with your actual string session
+
+CMD_HNDLR = "."  # Command handler, '.' in your case
+
+# Deployer Name
+DEPLOYER_NAME = "—͟͞͞Ⱥᴋᴋɪ IᴢPʀᴏ ꪜ『🇮🇳』"  # Replace with your name
