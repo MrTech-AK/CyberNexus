c6d6517c58ed93afbaaa7a893be654df14c40b5d
Akshat <162790900+MrTech-AK@users.noreply.github.com>
2025-02-08 09:27:05 +0530

    Update timetravel.py

--- plugins/timetravel.py
+++ plugins/timetravel.py
@@ -1,6 +1,11 @@
-import time
 from telethon import events
 from cybernexus import client
+import config
+import time
+import sys
+import telethon
+import platform  # ✅ Fixed import
+
 
 @client.on(events.NewMessage(pattern=r"^\.timetravel$", outgoing=True))
 def time_travel(event):
