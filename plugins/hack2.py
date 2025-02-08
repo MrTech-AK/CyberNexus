1460e728f74fb49af3db7d5c69e2432091958f54
Akshat <162790900+MrTech-AK@users.noreply.github.com>
2025-02-08 09:09:10 +0530

    Update hack2.py

--- plugins/hack2.py
+++ plugins/hack2.py
@@ -1,6 +1,10 @@
-import time
 from telethon import events
 from cybernexus import client
+import config
+import time
+import sys
+import telethon
+import platform  # ✅ Fixed import
 
 @client.on(events.NewMessage(pattern=r"^\.hack2$", outgoing=True))
 def fake_hack(event):
